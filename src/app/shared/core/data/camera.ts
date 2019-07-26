dc271968aa9cc510bcddeb284ed8cabc6f005978
tobv.tv <tobv@hitachiconsulting.com>
2019-07-26 18:28:30 +0700

    Update

--- src/app/shared/core/data/camera.ts
+++ src/app/shared/core/data/camera.ts
@@ -2,7 +2,8 @@ const CAMERA = [
     {
       id : '1',
       image: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Number13.png', 
-      camera: 'https://images.vexels.com/media/users/3/130358/isolated/preview/cbf8a4bf99f5c1687ba00adb5a9721a3-flat-video-camera-icon-by-vexels.png',
+      // camera: '../../../../assets/images/camera-red.png',
+      camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
       position : {
         width: '25px',
         height: '30px',
@@ -14,7 +15,7 @@ const CAMERA = [
     {
       id : '2',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZGbRs3hlZQmyeMhKwrzMsBJALr55loynODkxgz-lTXAeqRJA',
-      camera: 'https://images.vexels.com/media/users/3/130358/isolated/preview/cbf8a4bf99f5c1687ba00adb5a9721a3-flat-video-camera-icon-by-vexels.png',
+      camera: 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
       position:{
         width: '25px',
         height: '30px',
@@ -26,7 +27,7 @@ const CAMERA = [
     {
       id : '3', 
       image: 'https://banner2.kisspng.com/20180329/ffq/kisspng-number-haku-clip-art-number-1-5abc7083030507.7835562015222990110124.jpg', 
-      camera : 'https://images.vexels.com/media/users/3/130358/isolated/preview/cbf8a4bf99f5c1687ba00adb5a9721a3-flat-video-camera-icon-by-vexels.png',
+      camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
       position : {
         width: '25px',
         height: '30px',
