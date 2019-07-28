c9c42c2864706d7469603601b37a254847c4033e
Pet <vobichto1997@gmail.com>
2019-07-28 17:59:16 +0700

    Update

--- src/app/shared/core/data/camera.ts
+++ src/app/shared/core/data/camera.ts
@@ -2,8 +2,8 @@ const CAMERA = [
     {
       id : '1',
       image: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Number13.png', 
-      // camera: '../../../../assets/images/camera-red.png',
-      camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
+      camera: '../../../../assets/images/cam.png',
+      // camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
       position : {
         width: '25px',
         height: '30px',
@@ -15,7 +15,8 @@ const CAMERA = [
     {
       id : '2',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZGbRs3hlZQmyeMhKwrzMsBJALr55loynODkxgz-lTXAeqRJA',
-      camera: 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
+      // camera: 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
+      camera: '../../../../assets/images/cam.png',
       position:{
         width: '25px',
         height: '30px',
@@ -27,7 +28,8 @@ const CAMERA = [
     {
       id : '3', 
       image: 'https://banner2.kisspng.com/20180329/ffq/kisspng-number-haku-clip-art-number-1-5abc7083030507.7835562015222990110124.jpg', 
-      camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
+      // camera : 'https://cdn4.iconfinder.com/data/icons/devices-28/32/131-01-512.png',
+      camera: '../../../../assets/images/cam.png',
       position : {
         width: '25px',
         height: '30px',
